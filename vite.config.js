e6152e8c2d14175ea896a8387ada017b966c346f
Bryan Condor <bcondor@addi.com>
2026-05-10 10:43:42 -0500

    chore: configure GitHub Pages base URL for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lilian-mama/',
+  base: '/first-time-mom/',
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test-setup.js'],
